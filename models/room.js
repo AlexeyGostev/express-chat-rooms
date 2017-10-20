1cf507b7bb708e9bad3542e0fc66b04197ba8163
Alexey Gostev <AlGost96@mail.ru>
2017-10-20 21:14:51 +0300

    added method for work with Room-model (getRoom, createRoom, editRoom and deleteRoom)

--- models/room.js
+++ models/room.js
@@ -7,9 +7,13 @@ let schema = new Schema({
         unique: true,
         required: true
     },
-    name: {
+    title: {
         type: String,
         unique: true,
+        required: true
+    },
+    description : {
+        type: String,
         required: true
     },
     creator: {
@@ -25,5 +29,101 @@ let schema = new Schema({
         default: Date.now
     }
 });
+
+schema.static.createRoom = function(options) {
+    return new Promise((resolve, reject) => {
+        const Room = this;
+
+        let room = new Room({
+            id: options.id,
+            title: options.title,
+            description: options.description,
+            creator: options.creator || 'NoName'
+        });
+        room.save((err, room) => {
+            if (err) reject(err);
+            if (room) resolve(room);
+        });
+    });
+};
+
+schema.static.editRoom = function(editRoom) {
+    return new Promise((resolve, reject) => {
+        const Room = this;
+        let search = {};
+        if (editRoom.id) {
+            search.id = editRoom.id;
+        } else if (editRoom.title){
+            search.title = editRoom.title;
+            } else {
+                reject(new Error('Некорректный объект поиска'));
+                }
+
+        Room.findOne(search, (err, room) => {
+            if (err) {
+                log.error(err.message);
+                reject(err);
+            }
+
+            for ( let el in editRoom) {
+                if (editRoom.hasOwnProperty(el)) {
+                    room[el] = editRoom[el];
+                }
+            }
+
+            room.save((err, room) => {
+                if (err) {
+                    log.error(err.message);
+                    reject(err);
+                }
+                resolve(room);
+            });
+        });
+    });
+};
+
+schema.static.getRoom = function(id, title) {
+    return new Promise((resolve, reject) => {
+        const Room = this;
+        let search = {};
+        if (id) {
+            search.id = id;
+        } else if (title) {
+            search.title = title;
+            } else {
+                reject(new Error('Некорректный объект поиска'));
+                }
+
+        Room.findOne(search, (err, room) => {
+            if (err) {
+                log.error(err.message);
+                reject(err);
+            }
+            resolve(room);
+        });
+    });
+};
+
+schema.static.deleteRoom = function(id, title) {
+    return new Promise((resolve, reject) => {
+        const Room = this;
+        let search = {};
+        if (id) {
+            search.id = id;
+        } else if (title) {
+            search.title = title;
+            } else {
+                reject(new Error('Некорректный объект поиска'));
+                }
+        Room.remove(search, (err, room) => {
+            if (err) {
+                log.error(err.message);
+                reject(err);
+            }
+            resolve(room);
+        });
+    });
+};
+
 
 exports.Room = mongoose.model('Room', schema);
